5f2b51ce93b4acc2605079a045f41ceafb51bff9
Maciej M <22326971+madziejm@users.noreply.github.com>
2023-05-13 15:48:24 +0200

    Add draft model architecture

--- src/tests/test_classifier.py
+++ src/tests/test_classifier.py
@@ -5,6 +5,6 @@ from fontr.fontr.classifier import Classifier
 
 def test_classsifier_forward():
     lr = 0.01
-    _input = torch.rand(1, 3, 115, 115)
+    _input = torch.rand(1, 1, 96, 96)  # grayscale thus single channel
     classifier = Classifier(lr)
     classifier(_input)
